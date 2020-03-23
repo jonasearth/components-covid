ff2ce53b2c5d1ec0c01ccd603a217706874f88ac
jonasearth <38560805+jonasearth@users.noreply.github.com>
2020-03-23 05:17:22 -0300

    corrigindo

--- src/components/casos/Acumulados/Styles.js
+++ src/components/casos/Acumulados/Styles.js
@@ -1,31 +1,16 @@
 const Styles = {
-<<<<<<< HEAD
-    default:{
-        display:'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+    default: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
     },
-    formatting:{
-        backgroundColor: '#0002',
-        justifyContent: 'space-around',
+    formatting: {
+        backgroundColor: "#0002",
+        justifyContent: "space-around"
     },
     date: {
-        fontFamily: 'Arial',
+        fontFamily: "Arial"
     }
-=======
-  default:{
-      display:'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-  },
-  date: {
-      backgroundColor: '#0002',
-      flexDirection:'row',
-      width: '20px',
-      justifyContent: 'space-around'
-  }
->>>>>>> 78977821a6ae87bdec02fbab8b29061cf853895c
-}
+};
 export default Styles;
